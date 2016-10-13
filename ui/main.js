6ea8742b20bc4c55c79e2220c3efb7eae3df6fde
kullus <ks9634123781@gmail.com>
2016-10-13 14:28:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of main text
 var element=document.getElementById('main-txt');
 element.innerHTML='kuldeep';
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick=function() {
+    img.style.marginLeft=100px;
+};
