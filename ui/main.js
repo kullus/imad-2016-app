2c9b6dabc64a9e45f8bbc3d622c81263f66bcdc9
kullus <ks9634123781@gmail.com>
2016-10-14 16:35:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,21 @@
+//counter code
+
 var button=document.getElementById('counter');
 var counter=0;
+
 button.onclick=function(){
+//make a request to  counter endpoint
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.ready.State===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var counter=request.response.text;
+            }
+        }
+        //not done yet
+    }
     counter=counter+1;
     var span=getElementById('count');
     span.innerHTML=counter.toString();
